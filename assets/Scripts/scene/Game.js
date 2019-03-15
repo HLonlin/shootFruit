e0b1775e34bccfa23081e633e05674d11fbc3ec5
FHL <1021944439@qq.com>
2019-03-15 15:38:48 +0800

    关卡信息

--- assets/Scripts/scene/Game.js
+++ assets/Scripts/scene/Game.js
@@ -30,6 +30,18 @@ cc.Class({
 			type: cc.Node,
 			displayName: '障碍物',
 			tooltip: '第二个障碍物'
+		},
+		font_level: {
+			default: null,
+			type: cc.Label,
+			displayName: '关卡数',
+			tooltip: '关卡数'
+		},
+		font_levelTip: {
+			default: null,
+			type: cc.RichText,
+			displayName: '关卡提示',
+			tooltip: '关卡奖励子弹提示'
 		}
 	},
 	// 生命周期回调
@@ -54,6 +66,22 @@ cc.Class({
 		let hinders_animName = this.hinders.name + USERINFO.Data_game[2].json[USERINFO.level - 1].hinders;
 		var hinder_animState = hinder_anim.play(hinder_animName);
 		var hinders_animState = hinders_anim.play(hinders_animName);
+		// 关卡信息
+		this.font_level.string = '第' + USERINFO.level + '关';
+		for (var i = 0, max = USERINFO.Data_game[2].json.length; i < max; i++) {
+			if (USERINFO.Data_game[2].json[i].bullet != 0) {
+				if (USERINFO.level - 1 == i) {
+					this.font_levelTip.string = '<outline color=#223f67 width=2><color=#ffffff>(通过本关可获得新型子弹)</color></outline>';
+					break;
+				} else {
+					if (USERINFO.level - 1 < i) {
+						console.log(i);
+						this.font_levelTip.string = '<outline color=#223f67 width=2><color=#ffffff>(还差  ' + (i - (USERINFO.level - 1)) + '  关获得新型子弹)</color></outline>';
+						break;
+					}
+				}
+			}
+		}
 	},
 	changeScore: function (score) {
 		HL.nodePoolState.gameScore += score;
